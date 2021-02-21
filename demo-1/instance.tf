e81fbd1e621a945ce8506fcb7feb948c31e1884a
YongGan <YongGan@Yong-Gan-8.local>
2021-02-21 03:36:08 +0000

    update instance and var

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = var.AMIS[var.AWS_REGION]
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
 
--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,13 +5,13 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-07f5c641c23596eb9"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-0d729a60"
   }
