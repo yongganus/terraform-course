132dc3b8afe350de44062e2a737b332dff62c6d2
YongGan <YongGan@Yong-Gan-8.local>
2021-02-21 03:41:41 +0000

    update instance and var

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,8 +1,6 @@
-variable "AWS_ACCESS_KEY" {
-}
+variable "AWS_ACCESS_KEY" {}
 
-variable "AWS_SECRET_KEY" {
-}
+variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
   default = "us-east-1"
@@ -12,8 +10,9 @@ variable "AMIS" {
   type = map(string)
   default = {
     us-east-1 = "ami-07f5c641c23596eb9"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-2 = "ami-0c9c2203357e2568a"
+    us-west-1 = "ami-0147c8e5021fdbb71"
+    us-west-2 = "ami-091127048df1f044d"
   }
 }
 
